00f7c59831b35c7445ad80b245fc8affe1f1f246
Nihal Pandey <nihalpandey@farmart.co>
2021-06-20 19:48:52 +0530

    sticky scroll bug fixed

--- src/components/compiler/compiler.js
+++ src/components/compiler/compiler.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Compiler.css";
 
-import { ToastContainer,toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 import axios from "axios";
@@ -20,7 +20,7 @@ export default class Compiler extends Component {
     this.setState({ input: event.target.value });
   };
 
-  notify= (text)=>{
+  notify = (text) => {
     toast.success(text);
   }
 
@@ -33,7 +33,7 @@ export default class Compiler extends Component {
     this.setState({ class_name: event.target.value });
   };
 
-  copyCorrectedCode = ()=>{
+  copyCorrectedCode = () => {
     navigator.clipboard.writeText(this.state.output);
     this.notify("Corrected code copied !");
   }
@@ -41,20 +41,20 @@ export default class Compiler extends Component {
   submit = async (e) => {
     e.preventDefault();
     const req_body = {
-      code:encodeURIComponent("package example;\n"+this.state.input),
-      class_name:this.state.class_name,
-      test_cases:encodeURIComponent("package example;\n"+this.state.test_cases)
+      code: encodeURIComponent("package example;\n" + this.state.input),
+      class_name: this.state.class_name,
+      test_cases: encodeURIComponent("package example;\n" + this.state.test_cases)
     }
-    
-    const response = await axios.post("http://localhost:3001/submit_code_and_test_cases",req_body);
+
+    const response = await axios.post("http://localhost:3001/submit_code_and_test_cases", req_body);
     console.log(response);
-    this.setState({output:response.data.corrected_code});
+    this.setState({ output: response.data.corrected_code });
 
     let outputText = document.getElementById("output");
     outputText.innerHTML = "";
     outputText.innerHTML += "Creating Submission ...\n";
     console.log(this.state);
-    }
+  }
   render() {
     return (
       <>
@@ -91,7 +91,17 @@ export default class Compiler extends Component {
               onChange={this.updateClassName}
               className="form-control form-inline mb-2 language"
             />
+
+            <div >
+              <span className="badge badge-primary heading my-2 ">
+                <i className="fas fa-user fa-fw fa-md"></i> Enter test cases here
+              </span>
+              <br />
+              <textarea id="input" onChange={this.updateTestCases} value={this.state.test_cases}></textarea>
+            </div>
+
           </div>
+          
           <div className="col-5">
             <ToastContainer autoClose={2000} />
             <div className="cursor" onClick={this.copyCorrectedCode}>
@@ -100,17 +110,11 @@ export default class Compiler extends Component {
               </span>
             </div>
             <textarea id="output" value={this.state.output} disabled></textarea>
-
           </div>
+          
         </div>
 
-        <div className="mt-2 ml-5">
-          <span className="badge badge-primary heading my-2 ">
-            <i className="fas fa-user fa-fw fa-md"></i> Enter test cases here
-          </span>
-          <br />
-          <textarea id="input" onChange={this.updateTestCases} value={this.state.test_cases}></textarea>
-        </div>
+
       </>
     );
   }

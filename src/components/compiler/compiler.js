33a65dae3976e1bb7b08e6f1ed250374a82a27a1
Nihal Pandey <nihalpandey@farmart.co>
2021-06-20 21:17:23 +0530

    logs added

--- src/components/compiler/compiler.js
+++ src/components/compiler/compiler.js
@@ -12,12 +12,15 @@ export default class Compiler extends Component {
       input: ``,
       output: ``,
       class_name: ``,
+      logs:"",
       test_cases: ``,
+      submit_disabled:false,
+      output_color:"grey"
     };
   }
   input = (event) => {
     event.preventDefault();
-    this.setState({ input: event.target.value });
+    this.setState({ input: event.target.value,output:"",logs:"",output_color:"grey" });
   };
 
   notify = (text) => {
@@ -26,11 +29,11 @@ export default class Compiler extends Component {
 
   updateTestCases = (event) => {
     event.preventDefault();
-    this.setState({ test_cases: event.target.value });
+    this.setState({ test_cases: event.target.value,output:"",logs:"",output_color:"grey" });
   };
   updateClassName = (event) => {
     event.preventDefault();
-    this.setState({ class_name: event.target.value });
+    this.setState({ class_name: event.target.value,output:"",logs:"",output_color:"grey" });
   };
 
   copyCorrectedCode = () => {
@@ -40,20 +43,30 @@ export default class Compiler extends Component {
 
   submit = async (e) => {
     e.preventDefault();
+    this.setState({submit_disabled:true});
     const req_body = {
       code: encodeURIComponent("package example;\n" + this.state.input),
       class_name: this.state.class_name,
       test_cases: encodeURIComponent("package example;\n" + this.state.test_cases)
     }
 
-    const response = await axios.post("http://localhost:3001/submit_code_and_test_cases", req_body);
-    console.log(response);
-    this.setState({ output: response.data.corrected_code });
+    try{
+      const response = await axios.post("http://localhost:3001/submit_code_and_test_cases", req_body);
+      console.log(response);
+      if(response.data.error){
+        this.setState({output:response.data.error,output_color:"red", logs:response.data.error})
+      }
+      else{
+        console.log(response.data);
+        this.setState({ output: response.data.corrected_code,output_color:"green", logs:response.data.logs });
+      }
+    }
+    catch(err){
+      this.setState({ output: err.message,color:"red" });
+    }
 
-    let outputText = document.getElementById("output");
-    outputText.innerHTML = "";
-    outputText.innerHTML += "Creating Submission ...\n";
-    console.log(this.state);
+    this.setState({submit_disabled:false})
+    //console.log(this.state);
   }
   render() {
     return (
@@ -79,6 +92,7 @@ export default class Compiler extends Component {
               type="submit"
               className="btn btn-danger ml-2 mr-2 "
               onClick={this.submit}
+              disabled = {this.state.submit_disabled}
             >
               <i className="fas fa-cog fa-fw"></i> Repair
             </button>
@@ -105,11 +119,19 @@ export default class Compiler extends Component {
           <div className="col-5">
             <ToastContainer autoClose={2000} />
             <div className="cursor" onClick={this.copyCorrectedCode}>
-              <span className="badge badge-info heading my-2 ">
+              <span className="badge badge-success heading my-2 ">
                 <i className="fas fa-exclamation fa-fw fa-md"></i> Corrected code
               </span>
             </div>
-            <textarea id="output" value={this.state.output} disabled></textarea>
+            <textarea className="output" value={this.state.output} disabled></textarea>
+            <br /> <br />
+
+            <div className="cursor">
+              <span className="badge badge-info heading my-2 ">
+                <i className="fas fa-exclamation fa-fw fa-md"></i> Logs
+              </span>
+            </div>
+            <textarea className="output mb-4" value={this.state.logs} disabled></textarea>
           </div>
           
         </div>
